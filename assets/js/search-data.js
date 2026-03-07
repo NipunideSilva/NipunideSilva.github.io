b05015bd65846ffaf1a62a98b14db6587d52bfa0
NipunideSilva <81766272+NipunideSilva@users.noreply.github.com>
2026-03-07 16:38:41 +0000

    Deploying to gh-pages from @ NipunideSilva/NipunideSilva.github.io@939ad30cb22f31658e621239014ddff37a6f1894 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -62,14 +62,9 @@ ninja.data = [{
             window.location.href = "/blog/2026/cognitive-guide/";
           
         },
-      },{id: "books-ikigai-the-japanese-secret-to-a-long-and-happy-life",
-          title: 'Ikigai: The Japanese Secret to a Long and Happy Life',
-          description: "A Japanese concept meaning &quot;reason for being&quot; — the book explores the  intersection of what you love, what you are good at, what the world needs,  and what you can be paid for.",
-          section: "Books",handler: () => {
-              window.location.href = "/books/ikigai/";
-            },},{id: "books-steve-jobs",
+      },{id: "books-steve-jobs",
           title: 'Steve Jobs',
-          description: "The exclusive biography of Steve Jobs, based on more than forty interviews  with Jobs conducted over two years, as well as interviews with more than  one hundred family members, friends, adversaries, competitors, and colleagues.",
+          description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/steve-jobs/";
             },},{id: "books-the-godfather",
@@ -77,6 +72,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
+            },},{id: "books-ikigai-the-japanese-secret-to-a-long-and-happy-life",
+          title: 'Ikigai: The Japanese Secret to a Long and Happy Life',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/ikigai/";
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
