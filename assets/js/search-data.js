b8fbdfa246864c03cc1e2b579829729fd5e8f54d
NipunideSilva <81766272+NipunideSilva@users.noreply.github.com>
2026-04-05 16:52:54 +0000

    Deploying to gh-pages from @ NipunideSilva/NipunideSilva.github.io@05a2ce4006dfc7534e533e7cd675532340245f08 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "members of the lab or group",
+          description: "Collaborators and advisors I work with",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
